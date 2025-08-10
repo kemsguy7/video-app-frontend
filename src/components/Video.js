035db71024cdc5fbb18773536f4425f87ee4f4b6
kemsguy7 <mattidungafa@gmail.com>
2025-08-10 23:59:28 +0100

    updates:  added prettierc file

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,23 +1,23 @@
 import React, { useRef, useState } from 'react'
-import './Video.css' 
+import './Video.css'
 
 const Video = () => {
-    const [ playing, setPlaying] = useState(false) 
-    const videoRef = useRef(null) 
+    const [playing, setPlaying] = useState(false)
+    const videoRef = useRef(null)
     const handleVideoPress = () => {
-        if(playing) {
-            videoRef.current.pause() 
+        if (playing) {
+            videoRef.current.pause()
             setPlaying(false)
         } else {
-            videoRef.current.play() 
+            videoRef.current.play()
             setPlaying(true)
         }
     }
     return (
-        <div className="video"> 
+        <div className="video">
             <video src="https://res.cloudinary.com/dxkxvfo2o/video/upload/
 v1608169738/video1_cvrjfm.mp4" className="video__player" loop ref={videoRef}
-onClick={handleVideoPress}></video>
+                onClick={handleVideoPress}></video>
         </div>
     )
 }
