612c10cb73e2b1fd59750b8ce734e1052741c159
kemsguy7 <mattidungafa@gmail.com>
2025-08-28 22:49:22 +0100

    updates: updated footer component

--- src/components/VideoFooter.js
+++ src/components/VideoFooter.js
@@ -1,14 +1,25 @@
 import React from 'react'
-
 import './VideoFooter.css' 
+import MusicNoteIcon from '@material-ui/icons/MusicNote'; 
+import Marquee from "react-fast-marquee";
 
 const VideoFooter = () => {
     return (
         <div className="videoFooter"> 
             <div className="videoFooter__text"> 
                 <h3> @Kemsguy7</h3> 
-                <p> This is a description of the video. </p>
+                <p> Home PC Setup </p>
+                <div className="videoFooter__ticker"> 
+                    <MusicNoteIcon className="videoFooter__icon" />
+                    <p> Music: Chill Vibes </p>
+                    <Marquee>
+                        <p>I am a Windows PC</p>
+                    </Marquee>
+                   
+                </div>
             </div>
+            <img className="videoFooter__record" src="https://static.
+thenounproject.com/png/934821-200.png" alt="video footer" />
         </div>
     )
 }
