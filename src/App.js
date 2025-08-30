02b01342aad7b4bac473555daf282572648173cf
kemsguy7 <mattidungafa@gmail.com>
2025-08-30 06:08:21 +0100

    updated app.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,26 @@ function App() {
   return (
     <div className="app"> 
       <div className="app__videos"> 
-        <Video /> 
-        <Video />
+        <Video 
+        url ="https://res.cloudinary.com/dxkxvfo2o/video/upload/
+v1608169738/video1_cvrjfm.mp4" 
+    channel ="kemsguy " 
+    description ="Home PC Setup"
+    song ="Chill Vibes"
+    likes ={100}
+    shares ={50}
+    messages ={25}
+/> 
+             <Video 
+        url ="https://res.cloudinary.com/dxkxvfo2o/video/upload/
+v1608169738/video1_cvrjfm.mp4" 
+    channel ="kemsguy " 
+    description ="Home PC Setup"
+    song ="Chill Vibes"
+    likes ={100}
+    shares ={50}
+    messages ={25}
+/> 
       </div>
     </div>
   );
